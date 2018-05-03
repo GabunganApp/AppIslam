1b93b874eb2f256ff0cf4f2f97ff8d0f65f79830
magfiraisdayanti <firaisdayanti@gmail.com>
2018-05-03 10:58:59 +0800

    Lanjutin

--- app/src/main/java/com/example/user/belajarapi/Doa6Activity.java
+++ app/src/main/java/com/example/user/belajarapi/Doa6Activity.java
@@ -1,6 +1,6 @@
 package com.example.user.belajarapi;
 
-        import android.content.Intent;
+import android.content.Intent;
         import android.os.Bundle;
         import android.support.v7.app.AppCompatActivity;
 
